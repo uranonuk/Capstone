a2fab03b5d97dddadf2c76be45cfadf81f4992ad
Uran Onuk <uran.onuk@mail.utoronto.ca>
2021-01-27 21:07:18 +0300

    Simpified ui.main
    There's now a method to create graphs

--- go.py
+++ go.py
@@ -7,22 +7,22 @@ import numpy as np
 import pyqtgraph
 import SWHear
 
-
-
 class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
     def __init__(self, parent=None):
         pyqtgraph.setConfigOption('background', 'w') #before loading widget
         
-        super(Window, self).__init__(parent)
+        super(ExampleApp, self).__init__(parent)
         
         # Setup
         self.setupUi(self)
+
         self.updatesPerSecond = 20
         self.rate = 44100
-        self.grFFT.plotItem.showGrid(True, True, 0.7)
+        
+        #self.grFFT.plotItem.showGrid(True, True, 0.7)
         self.grPCM.plotItem.showGrid(True, True, 0.7)
         #self.grSaw.plotItem.showGrid(True, True, 0.7)
-        self.inputbuffer.plotItem.showGrid(True, True, 0.7)
+        #self.inputbuffer.plotItem.showGrid(True, True, 0.7)
         self.maxFFT=0
         self.maxPCM=0
         #self.maxSaw=0
@@ -31,25 +31,27 @@ class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
         self.sawtooth = generate_wave('sawtooth')
         print(self.sawtooth)
 
+    
+
     def update(self):
         if not self.ear.data is None and not self.ear.fft is None:
             pcmMax=np.max(np.abs(self.ear.data))
             if pcmMax>self.maxPCM:
                 self.maxPCM=pcmMax
                 self.grPCM.plotItem.setRange(yRange=[-pcmMax,pcmMax])
-                self.inputbuffer.plotItem.setRange(yRange=[-pcmMax,pcmMax])
+                #self.inputbuffer.plotItem.setRange(yRange=[-pcmMax,pcmMax])
             if np.max(self.ear.fft)>self.maxFFT:
                 self.maxFFT=np.max(np.abs(self.ear.fft))
                 #self.grFFT.plotItem.setRange(yRange=[0,self.maxFFT])
-                self.grFFT.plotItem.setRange(yRange=[0,1])
-            self.pbLevel.setValue(1000*pcmMax/self.maxPCM)
+                #self.grFFT.plotItem.setRange(yRange=[0,1])
+            #self.pbLevel.setValue(1000*pcmMax/self.maxPCM)
             pen=pyqtgraph.mkPen(color='b')
             self.grPCM.plot(self.ear.datax,self.ear.data,pen=pen,clear=True)
             if self.ear.plotbuff:
                 pen=pyqtgraph.mkPen(color='g')
-                self.inputbuffer.plot(self.ear.databuffx,self.ear.databuff,pen=pen,clear=True)
+                #self.inputbuffer.plot(self.ear.databuffx,self.ear.databuff,pen=pen,clear=True)
             pen=pyqtgraph.mkPen(color='r')
-            self.grFFT.plot(self.ear.fftx,self.ear.fft/self.maxFFT,pen=pen,clear=True)
+            #self.grFFT.plot(self.ear.fftx,self.ear.fft/self.maxFFT,pen=pen,clear=True)
             #pen=pyqtgraph.mkPen(color='g')
             #self.grSaw.plot(self.sawtooth.get_frame(0))  
         QtCore.QTimer.singleShot(1, self.update) # QUICKLY repeat
@@ -63,3 +65,166 @@ def run():
     form.ear.keepRecording=False
     print("DONE")
     sys.exit()
+
+if __name__=="__main__":
+    run()
+
+'''
+from PyQt5 import QtGui,QtCore
+
+import sys
+import ui_main
+import numpy as np
+import pyqtgraph
+import SWHear
+
+class Window(QtGui.QMainWindow, ui_main.Ui_MainWindow):
+    
+    # Core initial app structure
+
+    def __init__(self, parent=None):
+        pyqtgraph.setConfigOption('background', 'w') #before loading widget
+        
+        super(Window, self).__init__(parent)
+        
+        # Setup
+        self.setupUi(self)
+        
+        # bug: Causes quit button to not work
+        
+        # Plotting initial screen
+        self.grFFT.plotItem.showGrid(True, True, 0.7)
+        self.grPCM.plotItem.showGrid(True, True, 0.7)
+        
+        self.maxFFT=0
+        self.maxPCM=0
+        self.ear = SWHear.SWHear(rate=44100,updatesPerSecond=20)
+        self.ear.stream_start()
+        
+        
+        # Main menu bar
+        extractAction = QtGui.QAction("&Force Quit", self) # Name
+        extractAction.setShortcut("Ctrl+Q") # Shortcut
+        extractAction.triggered.connect(self.close_application) # Event when pressed
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(extractAction)
+
+
+        self.home()
+        
+    # Home page
+    def home(self):
+        
+        
+        quit_btn = QtGui.QPushButton("Quit", self)
+        quit_btn.clicked.connect(self.close_application)
+
+        quit_btn.resize(quit_btn.minimumSizeHint())
+        quit_btn.move(0,100)
+
+        # Toolbar
+        extractAction = QtGui.QAction("abc",self)
+        extractAction.triggered.connect(self.close_application)
+
+        self.toolBar = self.addToolBar("Extraction")
+        self.toolBar.addAction(extractAction)
+
+        # CheckBox
+        checkBox = QtGui.QCheckBox('Enlarge Window', self)
+        # checkBox.toggle() # Changes the default toggle setting
+        checkBox.stateChanged.connect(self.enlarge_window)
+        
+        
+        # Progress Bar
+        self.progress = QtGui.QProgressBar(self)
+        self.progress.setGeometry(200,80,250,20)
+
+        self.quit_btn = QtGui.QPushButton("Download", self)
+        self.quit_btn.move(200,120)
+        self.quit_btn.clicked.connect(self.download)
+        
+        
+        # Drop Downs & Styles
+        print(self.style().objectName())
+        self.styleChoice = QtGui.QLabel("Windows", self)
+
+        comboBox = QtGui.QComboBox(self)
+        comboBox.addItem("motif")
+        comboBox.addItem("Windows")
+        comboBox.addItem("cde")
+        comboBox.addItem("Plastique")
+        comboBox.addItem("Cleanlooks")
+        comboBox.addItem("windowsvista")
+
+        comboBox.move(50, 250)
+        self.styleChoice.move(50, 150)
+        comboBox.activated[str].connect(self.style_choice)
+        
+
+        self.show()
+
+    def style_choice(self, text):
+        self.styleChoice.setText(text)
+        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create(text)) 
+    
+    def download(self):
+        self.completed = 0
+
+        while self.completed < 100:
+            self.completed += 0.001
+            self.progress.setValue(self.completed)
+    
+    def enlarge_window(self, state):
+        if state == QtCore.Qt.Checked:
+            self.setGeometry(50,50,1000,600)
+        else:
+            self.setGeometry(50,50,500,300)
+
+    # Closing method
+    def close_application(self):
+        # Pop up message before quitting
+        choice = QtGui.QMessageBox.question(self, 'Extract',
+        'Are you sure you want to quit?',
+        QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+        if choice == QtGui.QMessageBox.Yes:
+            sys.exit()
+        else:
+            pass
+
+    def update(self):
+        if not self.ear.data is None and not self.ear.fft is None:
+            pcmMax=np.max(np.abs(self.ear.data))
+            if pcmMax>self.maxPCM:
+                self.maxPCM=pcmMax
+                self.grPCM.plotItem.setRange(yRange=[-pcmMax,pcmMax])
+            if np.max(self.ear.fft)>self.maxFFT:
+                self.maxFFT=np.max(np.abs(self.ear.fft))
+                #self.grFFT.plotItem.setRange(yRange=[0,self.maxFFT])
+                self.grFFT.plotItem.setRange(yRange=[0,1])
+            self.pbLevel.setValue(1000*pcmMax/self.maxPCM)
+            pen=pyqtgraph.mkPen(color='b')
+            self.grPCM.plot(self.ear.datax,self.ear.data,pen=pen,clear=True)
+            pen=pyqtgraph.mkPen(color='r')
+            self.grFFT.plot(self.ear.fftx,self.ear.fft/self.maxFFT,pen=pen,clear=True)
+        QtCore.QTimer.singleShot(1, self.update) # QUICKLY repeat
+
+def run():
+    app = QtGui.QApplication(sys.argv)
+    window = Window()
+    # window.show()
+    # window.update() #start with something
+    sys.exit(app.exec_())
+    print("DONE")
+
+if __name__=="__main__":
+    run()
+    
+    
+    
+    
+'''
--- ui_main.py
+++ ui_main.py
@@ -25,15 +25,29 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(993, 692)
+        MainWindow.resize(500, 692)
         
-        # Create centralwidget
+        # Create centralwidget (graph)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         
+        # Create paramwidget (params)
+        self.paramwidget = QtGui.QWidget(MainWindow)
+        self.paramwidget.setObjectName(_fromUtf8("paramwidget"))
+
         self.horizontalLayout = QtGui.QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         
+        self.frame = QtGui.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtGui.QFrame.NoFrame)
+        self.frame.setFrameShadow(QtGui.QFrame.Plain)
+        self.frame.setObjectName(_fromUtf8("frame"))
+
+        self.verticalLayout = QtGui.QVBoxLayout(self.frame)
+        self.verticalLayout.setContentsMargins(0,0,0,0)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+
+        '''
         # Progress Bar
         self.pbLevel = QtGui.QProgressBar(self.centralwidget)
         self.pbLevel.setMaximum(1000)
@@ -41,57 +55,76 @@ class Ui_MainWindow(object):
         self.pbLevel.setTextVisible(False)
         self.pbLevel.setOrientation(QtCore.Qt.Vertical)
         self.pbLevel.setObjectName(_fromUtf8("pbLevel"))
-        self.horizontalLayout.addWidget(self.pbLevel)
-        
-        self.frame = QtGui.QFrame(self.centralwidget)
-        self.frame.setFrameShape(QtGui.QFrame.NoFrame)
-        self.frame.setFrameShadow(QtGui.QFrame.Plain)
-        self.frame.setObjectName(_fromUtf8("frame"))
-        
-        self.verticalLayout = QtGui.QVBoxLayout(self.frame)
-        self.verticalLayout.setContentsMargins(0,0,0,0)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.label = QtGui.QLabel(self.frame)
-        self.label.setObjectName(_fromUtf8("label"))
-        
-        self.verticalLayout.addWidget(self.label)
-        
-        self.grFFT = PlotWidget(self.frame)
-        self.grFFT.setObjectName(_fromUtf8("grFFT"))
-        
-        self.verticalLayout.addWidget(self.grFFT)
+        #self.horizontalLayout.addWidget(self.pbLevel)
+        '''
 
-        self.label_2 = QtGui.QLabel(self.frame)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.verticalLayout.addWidget(self.label_2)
-        self.grPCM = PlotWidget(self.frame)
-        self.grPCM.setObjectName(_fromUtf8("grPCM"))
-        self.verticalLayout.addWidget(self.grPCM)
+        # Add Raw Data Graph
+        self.addGraph("PCM", "Raw Data (PCM)")
 
-        #self.label_3 = QtGui.QLabel(self.frame)
-        #self.label_3.setObjectName(_fromUtf8("label_3"))
-        #self.verticalLayout.addWidget(self.label_3)
-        #self.grSaw = PlotWidget(self.frame)
-        #self.grSaw.setObjectName(_fromUtf8("grSaw"))
-        #self.verticalLayout.addWidget(self.grSaw)
-        
+        # Add FFT graph
+        #self.addGraph("FFT", "Frequency Data (FFT)")
+
         #Add a window for displaying input buffer
-        self.label_3 = QtGui.QLabel(self.frame)
-        self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.verticalLayout.addWidget(self.label_3)
-        self.inputbuffer = PlotWidget(self.frame)
-        self.inputbuffer.setObjectName(_fromUtf8("Input Buffer"))
-        self.verticalLayout.addWidget(self.inputbuffer)
+        #self.addGraph("inputbuffer", "Input Buffer")
+        
+        self.addParamBox()
 
         self.horizontalLayout.addWidget(self.frame)
         MainWindow.setCentralWidget(self.centralwidget)
         
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        
+    # Creates a data graph
+    def addGraph(self, name, title, state=1):
+        label = name + "_label"
+        print("label: ", label)
+        print("name: ", name)
+        grName = "gr" + name
+
+        #self.label = QtGui.QLabel(self.frame)
+        setattr(self, label, QtGui.QLabel(self.frame))
+
+        #self.label.setObjectName(_fromUtf8("label"))  -> self.label becomes tmp
+        tmp = getattr(self, label)
+        tmp.setObjectName(_fromUtf8("label"))
+
+        self.verticalLayout.addWidget(tmp)
+
+        #self.grName = PlotWidget(self.frame)
+        setattr(self, grName, PlotWidget(self.frame))
+
+        #self.grName.setObjectName(_fromUtf8(grName))  -> self.grName becomes tmp2
+        tmp2 = getattr(self, grName)
+        tmp2.setObjectName(_fromUtf8(grName))
+        
+        if state:
+            self.verticalLayout.addWidget(tmp2)
+        else:
+            pass
+        
+        # RetranslateUi:
+        #self.label.setText(_translate("MainWindow", "frequency data (FFT):", None))
+        tmp.setText(_translate("MainWindow", title, None))
+
+    # Creates a parameter box
+    def addParamBox(self, state=1):
+        container = QtGui.QWidget()
+        container.setLayout(QtGui.QVBoxLayout())
+
+        # Buttons
+        ex = QtGui.QPushButton("example")
+        container.layout().addWidget(ex)
+        self.layout().addWidget(container)
+
+    # Creates a modes tab
+    def addModesTab(self, state=1):
+        pass
+    # Unused now
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
         self.label.setText(_translate("MainWindow", "frequency data (FFT):", None))
-        self.label_2.setText(_translate("MainWindow", "raw data (PCM):", None))
+        self.label_2.setText(_translate("MainWindow", "Raw Data (PCM):", None))
+        self.label_3.setText(_translate("MainWindow", "Input Buffer:", None))
 
 from pyqtgraph import PlotWidget
