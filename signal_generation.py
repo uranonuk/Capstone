2f68530858b3b57a941d08f1e9b9fcc8d55ee685
Johnathon Martin <johnathon.martin9@gmail.com>
2021-03-19 04:00:49 -0400

    added averaging function

--- signal_generation.py
+++ signal_generation.py
@@ -1,5 +1,7 @@
 from pydub import generators, utils, AudioSegment
 import numpy as np
+import math
+from random import rand
 
 #wave types include sine, pulse, square, sawtooth, triagle
 def generate_wave(wave_type, freq=10, bit_depth=16, duration=1000, volume=0):
@@ -17,3 +19,23 @@ def generate_wave(wave_type, freq=10, bit_depth=16, duration=1000, volume=0):
     wave = wave.get_array_of_samples()
     wave = np.array(wave)
     return wave
+
+def signal_averaging(signal, replicates):
+    N=len(signal)
+    even = np.zeros(N)
+    odd = np.zeros(N)
+    actual_noise = np.zeroes(N)
+    x = np.linspace(0, math.pi*4, N)
+    for i in range(1, replicates + 1):
+        noise = np.random.randn(N)
+        actual_noise = actual_noise+noise
+        if i%2:
+            even = even + noise + x
+        else:
+            odd = odd + noise + x
+    even_avg = even/(replicates/2)
+    odd_avg = odd/(replicates/2)
+    actual_avg = actual_noise/replicates
+    return np.sqrt(np.mean(actual_avg**2))
+
+        
